289df1a5b882d06f1abad0180ebb53123e41430c
Anqeliccom <hamidulinaanzelika462@gmail.com>
2025-02-24 18:46:53 +0700

    dynamic segment tree

--- nstt_1/SegmentTree.cpp
+++ nstt_1/SegmentTree.cpp
@@ -3,44 +3,63 @@
 
 class SegmentTree {
 private:
-    size_t n;
-    int* tree;
+    struct Node {
+        int value;
+        Node* left;
+        Node* right;
 
-    void build(size_t v, size_t tl, size_t tr, const int* nums) {
+        Node() {
+            value = 0;
+            left = nullptr;
+            right = nullptr;
+        }
+
+        ~Node() {
+            delete left;
+            delete right;
+        }
+    };
+
+    size_t n;
+    Node* root;
+
+    void build(Node*& node, size_t tl, size_t tr, const std::vector<int>& nums) {
+        node = new Node();
         if (tl == tr) {
-            tree[v] = nums[tl];
+            node->value = nums[tl];
+            return;
         } else {
             size_t tm = (tl + tr) >> 1;
-            build(v * 2, tl, tm, nums);
-            build(v * 2 + 1, tm + 1, tr, nums);
-            tree[v] = tree[v * 2] + tree[v * 2 + 1];
+            build(node->left, tl, tm, nums);
+            build(node->right, tm + 1, tr, nums);
+            node->value = node->left->value + node->right->value;
         }
     }
 
-    void updateRecursive(size_t v, size_t tl, size_t tr, size_t index, int val) {
+    void updateRecursive(Node* node, size_t tl, size_t tr, size_t index, int val) {
         if (tl == tr) {
-            tree[v] = val;
+            node->value = val;
+            return;
         } else {
             size_t tm = (tl + tr) >> 1;
             if (index <= tm) {
-                updateRecursive(v * 2, tl, tm, index, val);
+                updateRecursive(node->left, tl, tm, index, val);
             } else {
-                updateRecursive(v * 2 + 1, tm + 1, tr, index, val);
+                updateRecursive(node->right, tm + 1, tr, index, val);
             }
-            tree[v] = tree[v * 2] + tree[v * 2 + 1];
+            node->value = node->left->value + node->right->value;
         }
     }
 
-    int sumRecursive(size_t v, size_t tl, size_t tr, size_t left, size_t right) {
-        if (left > right) return 0;
-        if (left == tl && right == tr) return tree[v];
+    int sumRecursive(Node* node, size_t tl, size_t tr, size_t left, size_t right) {
+        if (left == tl && right == tr) return node->value;
         size_t tm = (tl + tr) >> 1;
         int res = 0;
         if (left <= tm) {
-            res += sumRecursive(v * 2, tl, tm, left, std::min(right, tm));
+            res += sumRecursive(node->left, tl, tm, left, std::min(right, tm));
         }
         if (right >= tm + 1) {
-            res += sumRecursive(v * 2 + 1, tm + 1, tr, std::max(left, tm + 1), right);
+            res += sumRecursive(node->right, tm + 1, tr, std::max(left, tm + 1), right);
         }
         return res;
     }
@@ -48,28 +67,29 @@ private:
 public:
     explicit SegmentTree(const std::vector<int>& nums) {
         n = nums.size();
-        if (n == 0) {
-            tree = nullptr; // if delete is called on a null pointer, it has no effect
-            return;
+        root = nullptr;
+        if (n != 0) {
+            build(root, 0, n - 1, nums);
         }
-        tree = new int[4 * n]{}; // without {} - trash in tree
-        build(1, 0, n - 1, &nums[0]);
     }
 
     void update(size_t index, int val) {
-        if (n == 0) {
-            std::cerr << "Error: update element in empty array!" << std::endl;
+        if (!root) {
+            std::cerr << "Error: update element in empty tree!" << std::endl;
             return;
         }
-        updateRecursive(1, 0, n - 1, index, val);
+        if (index >= n){
+            std::cerr << "Error: going beyond the boundaries of tree!" << std::endl;
+        }
+        updateRecursive(root, 0, n - 1, index, val);
     }
 
     int sumRange(size_t left, size_t right) {
-        if (n == 0 || left > right) return 0;
-        return sumRecursive(1, 0, n - 1, left, right);
+        if (!root || left > right || right >= n) return 0;
+        return sumRecursive(root, 0, n - 1, left, right);
     }
 
     ~SegmentTree() {
-        delete[] tree;
+        delete root;
     }
 };
