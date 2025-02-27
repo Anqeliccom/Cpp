0067f0a8c360853b2b94ad8805b6d47cd6157325
Yury Kareba <wwsh@gmail.com>
2025-02-27 21:25:31 +0700

    Review02: changes requested

--- nstt_2/Lines.cpp
+++ nstt_2/Lines.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 
 struct Point {
@@ -9,15 +10,36 @@ struct Point {
     }
 };
 
+/* PR: about encapsulation
+ * This line representation is much complicated thing than the point
+ * We can easily broke up invariants of this struct 
+ * (it has only one invariant - equation a * x + b * y + c = 0 corresponds to line in 2D space)
+ * First way - just create line like this Line(0.0, 0.0, 0.0)
+ * Given equation will be equation of plane in 2D space
+ * The same behavior will be in case of Line(p, p), where p - any point
+ * See test samples
+ */
 struct Line {
     double a, b, c;
 
+    /* PR: constructor fix
+     * Let's try to fix this encapsulation issue
+     * I saw that you used exceptions in your code,
+     * but throwing exceptions in constructors 
+     * is a little bit tricky 
+     * (but possible and good way (if it is written right)) thing 
+     * 
+     * I suggest you to avoid them, try to use `std::optional`
+     * and some OOP 
+     * Note: if you can't come up with solution - feel free to text me
+     */
     Line(const Point& p1, const Point& p2) {
         a = p2.y - p1.y;
         b = p1.x - p2.x;
         c = p2.x * p1.y - p1.x * p2.y;
     }
-
+    
+    /* PR: The same concern as above */ 
     Line(double a, double b, double c) {
         this->a = a;
         this->b = b;
@@ -27,6 +49,19 @@ struct Line {
     Point getIntersection(const Line& other) const {
         double det = a * other.b - other.a * b;
 
+        /* PR: floats are tricky
+         * comparing floating point numbers using '==' operator is almost always wrong
+         * For see that go to tests
+         *
+         * The usual way (and correct in some sence) to implement floats comparison 
+         * is creation of function that works in this way `a == b <=> |a - b| < eps`
+         * where eps is some precision of your computations
+         *
+         * Let's implement function that provides such functionallity.
+         * And then I suggest you to use implemented function to compare all floats in your code
+         *
+         * Note: if something is unclear - please write to me
+         */
         if (det == 0) {
             if (a * other.c == c * other.a && b * other.c == c * other.b) {
                 std::cerr << "The lines match." << std::endl;
--- nstt_2/LinesTest.cpp
+++ nstt_2/LinesTest.cpp
@@ -1,6 +1,8 @@
 #include <cassert>
-#include "Lines.cpp"
+#include "Lines.cpp" // Note: usually we don't include source files to code
 
+// Note: it might be useful to use google test framework
+// it is really easy to use with CMake (but only if you don't use MacOS)
 void runTests() {
     // Test 1: intersection
     Line l1(Point(0, 0), Point(2, 2)); // y = x
@@ -29,6 +31,41 @@ void runTests() {
     } catch (const std::runtime_error&) {
         std::cerr << "Parallel lines detected.\n";
     }
+
+    // Test 5: weird lines
+    try {
+
+        std::cout << "Test 5: weird lines " << std::endl;
+
+        Point p1(1, 1);
+        Point p2(1, 1);
+        Line plane(p1, p2);
+        std::cout << "equation of line: " << plane.a << "*x " << plane.b 
+            << "*y " << "+ " << plane.c << " = 0" << std::endl;
+
+        std::cout << "But actually it is not line!\n" << "This line is a plane!\n\n";
+
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl; 
+    }
+
+    // Test 6: tricky floats
+    try {
+
+        std::cout << "Test 6: tricky floats\n" << std::endl;
+
+        Line first(1.0 / 3, 2, 0);
+        Line second(0.1, 3.0 / 5, 1);
+
+        Point intersection = first.getIntersection(second);
+        std::cout << "Lines has intersection: (" << intersection.x << ", " 
+            << intersection.y << ")" << std::endl;
+
+        std::cout << "But math says that there is no intersection!\n" << std::endl;
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl; 
+    }
+
 
     std::cout << "Tests passed!" << std::endl;
 }
