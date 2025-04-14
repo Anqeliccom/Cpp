48fbbee65298b9666412eed371ff45eebf1eca5e
Anqeliccom <hamidulinaanzelika462@gmail.com>
2025-04-14 21:13:40 +0700

    nstt_9 added

---
+++ nstt_10/SegmentTree.hpp
@@ -0,0 +1,14 @@
+//
+// Created by angelica on 14.04.2025.
+//
+
+#ifndef TASKS_SEGMENTTREE_HPP
+#define TASKS_SEGMENTTREE_HPP
+
+
+class SegmentTree {
+
+};
+
+
+#endif //TASKS_SEGMENTTREE_HPP
---
+++ nstt_10/SegmentTreeTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by angelica on 14.04.2025.
+//
+
+#include "SegmentTreeTest.hpp"
