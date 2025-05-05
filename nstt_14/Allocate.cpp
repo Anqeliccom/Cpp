19e3b2895cd4d953fce0be619bb6af161a9297a4
Anqeliccom <hamidulinaanzelika462@gmail.com>
2025-05-05 22:37:29 +0700

    add alignment

--- nstt_14/Allocate.cpp
+++ nstt_14/Allocate.cpp
@@ -1,14 +1,28 @@
 #include <memory>
 #include <cassert>
 
+template<typename T>
+constexpr size_t align_up(size_t offset) {
+    return (offset + alignof(T) - 1) & ~(alignof(T) - 1);
+}
+
+template<typename... Types>
+constexpr size_t compute_total_size() {
+    size_t offset = 0;
+    ((offset = align_up<Types>(offset), offset += sizeof(Types)), ...);
+    return offset;
+}
+
 template<size_t SIZE, typename... Types>
-void allocate(void* memory, Types... args) requires ((std::is_copy_constructible<Types>::value && ...) && (SIZE >= (sizeof(Types) + ...))) {
-    char* ptr = static_cast<char*>(memory);
+void allocate(void* memory, Types... args) requires ((std::is_copy_constructible<Types>::value && ...) && (SIZE >= (compute_total_size<Types...>()))) {
+    char* base = static_cast<char*>(memory);
+    size_t offset = 0;
 
     auto alloc = [&](const auto& arg) {
         using Type = std::remove_reference_t<decltype(arg)>;
-        new (ptr) Type(arg);
-        ptr += sizeof(Type);
+        offset = align_up<Type>(offset);
+        new (base + offset) Type(arg);
+        offset += sizeof(Type);
     };
 
     (alloc(args), ...);
